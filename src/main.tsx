566876628cd52b87de5cb09ba4085f8ce7f045e7
QUIROZ SOLDAN MARIO ALBERTO <marioaquirozs@gmail.com>
2025-11-17 21:49:44 -0600

    Add main.tsx file with initial React setup and TrafficLight component

---
+++ src/main.tsx
@@ -0,0 +1,14 @@
+import { StrictMode } from 'react';
+import { createRoot } from 'react-dom/client';
+
+import { HooksApp } from './HooksApp';
+import { TrafficLight } from './01-useState/trafficLight';
+
+import './index.css';
+
+createRoot(document.getElementById('root')!).render(
+  <StrictMode>
+    {/* <HooksApp /> */}
+    <TrafficLight />
+  </StrictMode>,
+)
